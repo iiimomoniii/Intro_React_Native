d95c00cb9471dd0c2b6f6a54a6d9e253e15fbfe1
Natdanai <natdanai.thaiyala@gmail.com>
2024-02-13 11:02:17 +0700

    Update
    import image

--- screens/Splash.js
+++ screens/Splash.js
@@ -1,12 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, Image} from 'react-native';
+import React from 'react';
 
 const Splash = () => {
   return (
-    <View style={{ alignItems:'center' }}>
-    <Text style={{  fontSize:30 }}>Me Live Code ,Hello</Text>
-  </View>
-  )
-}
+    <View style={{height: '100%', alignItems : "center", backgroundColor : "#131414"}}>
+      <View style={{flex: 0.95, justifyContent : "center"}}>
+        <Image source={require('../../Movie/static/img/logo.png')} style={{ height:300, width:280 }}/>
+      </View>
+      <View style={{flex: 0.05}}>
+        <Text style={{ color:"white" }}>MyCompany</Text>
+      </View>
+    </View>
+  );
+};
 
-export default Splash
+export default Splash;
