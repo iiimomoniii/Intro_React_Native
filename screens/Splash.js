2166b053074fc684ca2662bf24c163c08306b406
Natdanai <natdanai.thaiyala@gmail.com>
2024-02-13 10:43:59 +0700

    Update
    create child component and import to main component

---
+++ screens/Splash.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Splash = () => {
+  return (
+    <View style={{ alignItems:'center' }}>
+    <Text style={{  fontSize:30 }}>Me Live Code ,Hello</Text>
+  </View>
+  )
+}
+
+export default Splash
