4e35ff1e14bb9d9d26d77d1192c55acc93eae148
Natdanai <natdanai.thaiyala@gmail.com>
2024-02-13 11:33:24 +0700

    Update
    -add navigation from Splash to Home
    npm install @react-navigation/native @react-navigation/native-stack
    npm install react-native-screens react-native-safe-area-context

--- App.tsx
+++ App.tsx
@@ -1,9 +1,29 @@
 import { View, Text } from 'react-native'
+
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import Splash from './screens/Splash'
+import Home from './screens/Home';
+const Stack = createNativeStackNavigator();
+
 const App = () => {
   return (
-    <Splash/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Splash"
+          component={Splash}
+          options={{ headerShown:false}} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{title: 'Welcome'}}
+        />
+        {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
--- screens/Splash.js
+++ screens/Splash.js
@@ -1,16 +1,32 @@
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, Pressable, Alert} from 'react-native';
 import React from 'react';
 
-const Splash = () => {
+const Splash = ({ navigation }) => {
+  //Pressable use for redirect Splash page to Home page
+
+  const onPress = () => {
+    navigation.navigate('Home')
+  }
+
   return (
-    <View style={{height: '100%', alignItems : "center", backgroundColor : "#131414"}}>
-      <View style={{flex: 0.95, justifyContent : "center"}}>
-        <Image source={require('../../Movie/static/img/logo.png')} style={{ height:300, width:280 }}/>
+    <Pressable onPress={onPress}>
+      <View
+        style={{
+          height: '100%',
+          alignItems: 'center',
+          backgroundColor: '#131414',
+        }}>
+        <View style={{flex: 0.95, justifyContent: 'center'}}>
+          <Image
+            source={require('../../Movie/static/img/logo.png')}
+            style={{height: 300, width: 280}}
+          />
+        </View>
+        <View style={{flex: 0.05}}>
+          <Text style={{color: 'white'}}>MyCompany</Text>
+        </View>
       </View>
-      <View style={{flex: 0.05}}>
-        <Text style={{ color:"white" }}>MyCompany</Text>
-      </View>
-    </View>
+    </Pressable>
   );
 };
 
